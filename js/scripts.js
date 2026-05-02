5ea9a7b18d8cc1754d707b4788d3b9e8e1422d8c
Ajax Tchakounte <atchakou@iphone-2.myfiosgateway.com>
2026-05-02 17:21:00 -0400

    added js dir

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+function preventScroll() {
+  document.querySelector("#form-toggler").focus({ preventScroll: true });
+}
+
+document
+  .querySelector("[for=form-toggler]")
+  .addEventListener("click", preventScroll);
